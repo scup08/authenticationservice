c7104f9e859cefe87d374e82f7a6afcadffb0571
scup08 <519370424@qq.com>
2017-06-30 00:07:21 +0800

    jwt

--- src/main/java/com/xhld/authenticationservice/model/UserDto.java
+++ src/main/java/com/xhld/authenticationservice/model/UserDto.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class UserDto {
 	
     private String id;
-    private String userName;
+    private String username;
     private String password;
     private String email;
     private Date lastPasswordResetDate;
@@ -18,11 +18,11 @@ public class UserDto {
 	public void setId(String id) {
 		this.id = id;
 	}
-	public String getUserName() {
-		return userName;
+	public String getUsername() {
+		return username;
 	}
-	public void setUserName(String userName) {
-		this.userName = userName;
+	public void setUsername(String username) {
+		this.username = username;
 	}
 	public String getPassword() {
 		return password;
